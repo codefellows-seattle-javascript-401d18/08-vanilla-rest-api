98ba2a7966359199dd4e3980e1202c55f391bfb2
Shaun Wahle <shaun.wahle@gmail.com>
2017-08-30 14:22:33 -0700

    working on put

--- shaun-lab8/route/route-toy.js
+++ shaun-lab8/route/route-toy.js
@@ -47,6 +47,7 @@ module.exports = function(router) {
     res.end()
   })
 }
+
 route.delete('/api/toy', (req, res) => {
   debug('/api/toy DELETE');
   if(req.url.query._id) {
@@ -65,5 +66,26 @@ route.delete('/api/toy', (req, res) => {
   }
   res.writeHead(400, {'Content-Type': 'text.plain'});
   res.write('bad request; item id required to get record');
+  res.end();
+}
+
+router.put('/api/toy', (req, res) => {
+  debug('api/toy/PUT');
+  if(req.url.query._id) {
+    storage.updateItem('toy', req.url.query._id);
+    .then(toy => {
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end();
+    })
+    .catch(err => {
+      console.error(err);
+      res.writeHead(400, {'Content-Type': 'text/plain'});
+      res.write('bad request could not find record');
+      res.end();
+    })
+    return;
+  }
+  res.writeHead(400, {'Content-Type': 'text/plain'});
+  res.write('bad request; item update require ID');
   res.end();
 }
