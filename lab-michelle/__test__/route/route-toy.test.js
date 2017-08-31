536cfbe23d78c510a53fddb3948e60c3d638a351
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-08-30 21:56:16 -0700

    TESTS WHAT THE HECK

--- lab-michelle/__test__/route/route-toy.test.js
+++ lab-michelle/__test__/route/route-toy.test.js
@@ -3,7 +3,7 @@
 //
 // const routetoy = require('../route/route-toy.js');
 const superagent = require('superagent');
-const server = require('../server.js');
+const server = require('../../server.js');
 //1pt: write as many unit tests as you can to cover all of the modularized code in our lib, model, and route dirs
 
 // we gotta close the server after we test//
@@ -16,7 +16,7 @@ describe('We have a server instance', function() {
   // write a test to ensure that your api returns a status code of 404 for routes that have not been registered//
   describe('Someone puts in a route that has not been registered', ()=> {
     test('should give a 404', done => {
-      superagent.get(':3000')
+      superagent.get(':3000/api/toy') //trying this doesn't seem to help either
         .end((err, res) => {
           expect(res.status).toBe(404);expect(res.text).toBe('route not found');
           done();
@@ -25,9 +25,9 @@ describe('We have a server instance', function() {
   });
   //GET: test 404, it should respond with 'not found' for valid requests made with an id that was not found
   describe('for api/toy/ endpoint', ()=> {
-    describe('testing GET', done => {
+    describe('testing POST', done => {
       test('should give a 404', () => {
-        superagent.get(':3000')
+        superagent.post(':3000/api/toy')
           .end((err, res) => {
             expect(res.status).toBe(404);
             // expect(res.text).toBe('not found');
