0bb64819d42aa47120397256e5977fc5a80313e1
Tim Turner <timmyturner@me.com>
2017-08-30 22:01:11 -0700

    adds in tests but they arent working yet

--- lab-tim/__test__/server.test.js
+++ lab-tim/__test__/server.test.js
@@ -2,8 +2,6 @@
 
 const server = require('../server.js');
 const superagent = require('superagent');
-const cowsay = require('cowsay');
-
 
 describe('Testing the server file', function () {
   afterAll((done) => {
@@ -11,20 +9,22 @@ describe('Testing the server file', function () {
   });
 
   describe('POST  method', () => {
-    test('POST on /cowsay endpoint - should return status code 200', done => {
-      superagent.post('localhost:3000/cowsay')
-        .send({'text': 'hello'})
+    test('POST on /api/toy endpoint - should return status code 201 and response', done => {
+      superagent.post('localhost:3000/api/toy')
+        .send({name: 'slinky', desc: 'metal'})
         .set('Content-Type', 'application/json')
         .end((err, res) => {
           expect(err).toBeNull();
-          expect(res.status).toBe(200);
+          expect(res.status).toBe(201);
+          expect(res.body.name).toEqual('slinky');
+          expect(res.body.desc).toEqual('metal');
           done();
         });
     });
 
-    test('POST on /cowsay endpoint - should return status code 400', done => {
-      superagent.post('localhost:3000/cowsay')
-        .send({'name': 'hello'})
+    test('POST on /api/toy endpoint - should return status code 400', done => {
+      superagent.post('localhost:3000/api/toy')
+        .send({desc: 'orange'})
         .set('Content-Type', 'application/json')
         .end((err, res) => {
           expect(err).not.toBeNull();
@@ -32,54 +32,82 @@ describe('Testing the server file', function () {
           done();
         });
     });
+  });
 
-    test('POST on /cowsay endpoint -  should return cowsay dragon message', done => {
-      superagent.post('localhost:3000/cowsay')
-        .send({'text': 'hello'})
+  describe('PUT method', () => {
+    test.only('PUT on /api/toy endpoint - should return status code 204 and response', done => {
+      superagent.put('localhost:3000/')
+        .send({_id: 'some-id-string', name: 'slinky', desc: 'metal'})
         .set('Content-Type', 'application/json')
         .end((err, res) => {
           expect(err).toBeNull();
-          expect(res.text).toEqual(cowsay.say({text: 'hello', f: 'dragon'}));
+          expect(res.status).toBe(204);
+          expect(res.body._id).not.toBeNull();
+          expect(res.body.name).toEqual('slinky');
+          expect(res.body.desc).toEqual('metal');
           done();
         });
     });
 
-    test('POST on / endpoint - should return status code 200', done => {
-      superagent.post('localhost:3000/')
-        .send({'text': 'hello'})
+    test('PUT on /api/toy endpoint - should return status code 400', done => {
+      superagent.put('localhost:3000/api/toy')
+        .send({name: 'slinky', desc: 'metal'})
         .set('Content-Type', 'application/json')
         .end((err, res) => {
-          expect(err).toBeNull();
-          expect(res.status).toBe(200);
-          done();
-        });
-    });
-
-    test('POST on / endpoint - should return hello message', done => {
-      superagent.post('localhost:3000/')
-        .send({'text': 'hello'})
-        .set('Content-Type', 'application/json')
-        .end((err, res) => {
-          expect(err).toBeNull();
-          expect(res.text).toEqual('hello from my server!');
+          expect(err).not.toBeNull();
+          expect(res.status).toBe(400);
           done();
         });
     });
   });
 
   describe('GET method', () => {
-    test('GET on /cowsay endpoint - should return status code 200', done => {
-      superagent.get('localhost:3000/cowsay?text=hello')
+    test('GET on /api/toy endpoint - should return status code 200 and response', done => {
+      superagent.get('localhost:3000/api/toy?_id=some-id-string')
         .set('Content-Type', 'application/json')
         .end((err, res) => {
           expect(err).toBeNull();
           expect(res.status).toBe(200);
+          expect(res.body.name).toEqual('slinky');
+          expect(res.body.desc).toEqual('metal');
           done();
         });
     });
 
-    test('GET on /cowsay endpoint - should return status code 400', done => {
-      superagent.get('localhost:3000/cowsay?name=hello')
+    test('GET on /api/toy endpoint - should return status code 404', done => {
+      superagent.get('localhost:3000/api/toy?_id=some-other-id-string')
+        .set('Content-Type', 'application/json')
+        .end((err, res) => {
+          expect(err).not.toBeNull();
+          expect(res.status).toBe(404);
+          done();
+        });
+    });
+
+    test('GET on /api/toy endpoint - should return status code 400', done => {
+      superagent.get('localhost:3000/api/toy?name=some-id-string')
+        .set('Content-Type', 'application/json')
+        .end((err, res) => {
+          expect(err).not.toBeNull();
+          expect(res.status).toBe(400);
+          done();
+        });
+    });
+  });
+
+  describe('DELETE method', () => {
+    test('DELETE on /api/toy endpoint - should return status code 204', done => {
+      superagent.delete('localhost:3000/api/toy?_id=some-id-string')
+        .set('Content-Type', 'application/json')
+        .end((err, res) => {
+          expect(err).toBeNull();
+          expect(res.status).toBe(204);
+          done();
+        });
+    });
+
+    test('DELETE on /api/toy endpoint - should return status code 400', done => {
+      superagent.delete('localhost:3000/api/toy?name=some-id-string')
         .set('Content-Type', 'application/json')
         .end((err, res) => {
           expect(err).not.toBeNull();
@@ -88,32 +116,12 @@ describe('Testing the server file', function () {
         });
     });
 
-    test('GET on /cowsay endpoint - should return cowsay dragon message', done => {
-      superagent.get('localhost:3000/cowsay?text=hello')
+    test('DELETE on /api/toy endpoint - should return status code 404', done => {
+      superagent.delete('localhost:3000/api/toy?_id=some-other-id-string')
         .set('Content-Type', 'application/json')
         .end((err, res) => {
-          expect(err).toBeNull();
-          expect(res.text).toEqual(cowsay.say({text: 'hello', f: 'dragon'}));
-          done();
-        });
-    });
-
-    test('GET on / endpoint - should return status code 200', done => {
-      superagent.get('localhost:3000/')
-        .set('Content-Type', 'application/json')
-        .end((err, res) => {
-          expect(err).toBeNull();
-          expect(res.status).toBe(200);
-          done();
-        });
-    });
-
-    test('GET on / endpoint - should return hello message', done => {
-      superagent.get('localhost:3000/')
-        .set('Content-Type', 'application/json')
-        .end((err, res) => {
-          expect(err).toBeNull();
-          expect(res.text).toEqual('hello from my server!');
+          expect(err).not.toBeNull();
+          expect(res.status).toBe(404);
           done();
         });
     });
